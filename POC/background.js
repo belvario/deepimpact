6b9caff9c0958536cd69ddc692337ad9fd015a83
Andrew Evans <aevans.wv@gmail.com>
2019-11-24 22:46:43 +0100

    WIP on POC
    JQuery is not working for some reason

---
+++ POC/background.js
@@ -0,0 +1,9 @@
+//background.js
+chrome.runtime.onMessage.addListener(
+ function(request, sender, sendResponse) {
+   if( request.message === "open_max_url" ) {
+     fullURL = "http://" + request.url;
+     chrome.tabs.create({"url": fullURL, "active": false});
+   }
+ }
+);
